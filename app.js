7e8831981559fb17f9e943ff501303a2270e4b35
Rishi Guptha <rishi2002guptha@gmail.com>
2024-09-09 23:55:20 +0530

    names

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // @ts-nocheck
 
-const johnSelectorBtn = document.querySelector('#john-selector')
-const janeSelectorBtn = document.querySelector('#jane-selector')
+const johnSelectorBtn = document.querySelector('#rishi-selector')
+const janeSelectorBtn = document.querySelector('#aryan-selector')
 const chatHeader = document.querySelector('.chat-header')
 const chatMessages = document.querySelector('.chat-messages')
 const chatInputForm = document.querySelector('.chat-input-form')
@@ -11,7 +11,7 @@ const clearChatBtn = document.querySelector('.clear-chat-button')
 const messages = JSON.parse(localStorage.getItem('messages')) || []
 
 const createChatMessageElement = (message) => `
-  <div class="message ${message.sender === 'John' ? 'blue-bg' : 'gray-bg'}">
+  <div class="message ${message.sender === 'Rishi' ? 'blue-bg' : 'gray-bg'}">
     <div class="message-sender">${message.sender}</div>
     <div class="message-text">${message.text}</div>
     <div class="message-timestamp">${message.timestamp}</div>
@@ -24,18 +24,18 @@ window.onload = () => {
   })
 }
 
-let messageSender = 'John'
+let messageSender = 'Rishi'
 
 const updateMessageSender = (name) => {
   messageSender = name
   chatHeader.innerText = `${messageSender} chatting...`
   chatInput.placeholder = `Type here, ${messageSender}...`
 
-  if (name === 'John') {
+  if (name === 'Rishi') {
     johnSelectorBtn.classList.add('active-person')
     janeSelectorBtn.classList.remove('active-person')
   }
-  if (name === 'Jane') {
+  if (name === 'Aryan') {
     janeSelectorBtn.classList.add('active-person')
     johnSelectorBtn.classList.remove('active-person')
   }
@@ -44,8 +44,8 @@ const updateMessageSender = (name) => {
   chatInput.focus()
 }
 
-johnSelectorBtn.onclick = () => updateMessageSender('John')
-janeSelectorBtn.onclick = () => updateMessageSender('Jane')
+johnSelectorBtn.onclick = () => updateMessageSender('Rishi')
+janeSelectorBtn.onclick = () => updateMessageSender('Aryan')
 
 const sendMessage = (e) => {
   e.preventDefault()
